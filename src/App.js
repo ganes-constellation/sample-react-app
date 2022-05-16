fa27acd44b43d73c62ce33ee3d5124d2fe15054c
Ganeshrockz <ganeshrockz@github.com>
2022-05-16 15:43:42 +0530

    Fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			message: ""
+		};
+	}
+
+  componentDidMount() {
+		fetch(`https://ganes-node-server.herokuapp.com/`)
+		  .then(res => res.json())
+      .then(json => this.setState({ message: json['msg'] }));
+	}
+
+  render() {    
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
